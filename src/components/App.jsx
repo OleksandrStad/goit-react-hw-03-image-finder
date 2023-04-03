55f7cb987a3dab51a8ceb45b7423237c8b72276e
OleksandrStad <mmk.polymer@gmail.com>
2023-04-03 23:04:50 +0300

    add spinner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
-import { getImages } from '../services/getImages'
+import { getImages } from '../services/getImages';
+import { InfinitySpin } from 'react-loader-spinner';
 import { Searchbar } from './Searchbar/Searchbar';
-import { ImageGallery } from './ImageGallery/ImageGallery'
+import { ImageGallery } from './ImageGallery/ImageGallery';
 
 
 export class App extends Component {
   state = {
     images: [],
     nameSearch: '',
+    isLoading: false,
   }
 
   componentDidUpdate(prevProps, prevState) {
 
     if (prevState.nameSearch !== this.state.nameSearch) {
+
+      this.setState({ isLoading: true });
+
       getImages(this.state.nameSearch)
         .then(res => res.json())
         .then((images) => this.setState({ images: images.hits }))
+        .finally(() => {
+          this.setState({ isLoading: false })
+        });
+
+
     }
   };
 
@@ -28,11 +38,28 @@ export class App extends Component {
 
 
   render() {
+    const { images, isLoading } = this.state
+
     return (
-      <div>
+      <>
+
+
         <Searchbar onSubmit={this.handleChangeSubmit} />
-        <ImageGallery images={this.state.images} />
-      </div >
+
+
+        {isLoading && (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <InfinitySpin width="400" color="#4c2ef7" />
+          </div>
+        )}
+        <ImageGallery images={images} />
+      </>
     );
   };
 
